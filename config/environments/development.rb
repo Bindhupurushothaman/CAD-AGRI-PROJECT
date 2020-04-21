273e43754c342a3f4ee352ef2563a17e6ecd31df
Bindhupurushothaman <bindhupurushothaman23@gmail.com>
2020-04-21 16:09:46 +0100

    rb change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,18 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  #config.action_mailer.perform_deliveries = true
+  #config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com", 
+    port: 587,
+    authentication: "plain", 
+    enable_starttls_auto: true
+  }
 #default url
 config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
