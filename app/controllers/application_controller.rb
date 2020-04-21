9905061e0fd83ea96c37ad0228eac29ca6b11b86
Bindhupurushothaman <bindhupurushothaman23@gmail.com>
2020-04-21 15:13:36 +0100

    sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ end
 #permitting few attributes in the devise default parameters
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:update, keys: [:first_name, :last_name, :phone, :url])
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
     end	
 
 end
