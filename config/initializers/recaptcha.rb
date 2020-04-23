46d8f641b4819f822760316030edcb0f4574215a
Bindhupurushothaman <bindhupurushothaman23@gmail.com>
2020-04-23 20:32:17 +0100

    recaptchacheck

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-	config.site_key="6LeWX-0UAAAAAO5g8s9G-xsyJ-hKO-m6v9xVEkqp"
-	config.secret_key="6LeWX-0UAAAAAEWrYzy8wPdWqs3mU4AKZnPOrmfH"
+	config.site_key="6LfGYe0UAAAAADkqdDf2VhrvTevNUvGoMKyKmPAG"
+	config.secret_key="6LfGYe0UAAAAAMecSrzl_ekKTE8Hr0TJVz-oNbOi"
 end
