26894f589531ccec14a005dde04eb512dc03a96c
Bindhupurushothaman <bindhupurushothaman23@gmail.com>
2020-04-24 16:01:38 +0100

    security changes

--- db/migrate/20200414012040_create_profiles.rb
+++ db/migrate/20200414012040_create_profiles.rb
@@ -1,9 +1,10 @@
 class CreateProfiles < ActiveRecord::Migration[5.2]
+	
   def change
     create_table :profiles do |t|
-      t.string :firstname
-      t.string :lastname
-      t.string :address
+      t.string :firstname, null: false, default: ""
+      t.string :lastname, null: false, default: ""
+      t.string :address, null: false, default: ""
       t.references :user, foreign_key: true
 
       t.timestamps
